790b5f12e33d80eea2ffed209fa3ec7bf520f743
EC2 Default User <ec2-user@ip-172-31-40-41.us-east-2.compute.internal>
2017-06-22 00:27:41 +0000

    k

--- public/main.js
+++ public/main.js
@@ -2,18 +2,32 @@
 
   var socket = io();
   var C = new Audio('C_64kb.mp3');
-  C.load();
+  var D = new Audio('D_64kb.mp3');
+   var sound= new Audio('C_64kb.mp3'); 
+  sound.load();
 console.log(C);
  var current = {
     chord: 'C'
   };
 window.addEventListener("click", onClick,false);
+window.addEventListener("keydown",onKeyDown,false);
 
+function onKeyDown(event) {
+  var keyCode = event.keyCode;
+  switch (keyCode) {
+      case 68: //d
+       sound = D;
+      break;
+      case 67: //c
+	 sound =C;
+	break;
+ }
+}
 function play()
 {
-C.pause();
-C.load();
-C.play();
+sound.pause();
+sound.load();
+sound.play();
 }
 function onClick()
 {
